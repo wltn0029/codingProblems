9ba11454f894a1a5b402eab86858156236567b48
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-04-24 19:21:41 +0900

    pass in 68ms
    bug point :
    - vertex is repeated because of the data's form
    - cycle exists in bfs since visited is not managed

--- samsung/marbleMove.py
+++ samsung/marbleMove.py
@@ -3,117 +3,71 @@ N = int(a[0])
 M = int(a[1])
 board = [[] for i in range(N)]
 # pos (x, y)
-Rpos = (0, 0)
-Bpos = (0, 0)
-Opos = (0, 0)
+rx, ry, bx, by, ox, oy= [0]*6
 parent = dict()
 R = {}
 B = {}
 queue = []
-visited = []
+visited = {}
 
 # read input data
 for i in range(N):
     board[i] = list(raw_input())
     if "R" in board[i] :
         j = board[i].index("R")
-        Rpos = (i,j)
+        rx, ry = (i,j)
     if "B" in board[i] : 
         j = board[i].index("B")
-        Bpos = (i,j)
+        bx, by = (i,j)
     if "O" in board[i] : 
         j = board[i].index("O")
-        Opos = (i,j)
-
-# direction : up, down, left, right
-up = -1
-down = 1
-left = 2
-right = -2
-null = 0
+        ox, oy = (i,j)
 
 # board component : R, B, O, #, .
 block = "#"
 path = "."
+# direction : up, down, right, left
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def calMarblePos(curPos, otherPos, di) :
-    i, j = curPos
-    whileCheck = True
-    iUpdate = i 
-    jUpdate = j
-    iUpdate2 = i
-    jUpdate2 = j
-    if di == up :
-        whileCheck = i > 0
-        iUpdate = lambda x : x - 1
-        jUpdate = lambda y : y
-        iUpdate2 = lambda x : x + 1
-        jUpdate2 = jUpdate
-    elif di == down : 
-        whileCheck = i < N-2
-        iUpdate = lambda x : x + 1
-        jUpdate = lambda y : y
-        iUpdate2 = lambda x : x - 1
-        jUpdate2 = jUpdate
-    elif di == left : 
-        whileCheck = j > 0
-        iUpdate = lambda y : y
-        jUpdate = lambda x : x - 1
-        iUpdate2 = iUpdate
-        jUpdate2 = lambda x : x + 1
-    elif di == right : 
-        whileCheck = j < M-2
-        iUpdate = lambda y : y
-        jUpdate = lambda x : x + 1
-        iUpdate2 = iUpdate
-        jUpdate2 = lambda x : x - 1
-    else : 
-        return curPos
+def calMarblePos(rx, ry, dx, dy) :
+    c = 0
+    while board[rx+dx][ry+dy] != block and board[rx][ry] != "O" :
+        rx += dx
+        ry += dy
+        c += 1
+    return (rx, ry, c)
     
-    while whileCheck :
-        i2 = iUpdate(i)
-        j2 = jUpdate(j)
-        if board[i2][j2] == block : break
-        elif i2 == otherPos[0] and j2 == otherPos[1]:
-            otherX, otherY = calMarblePos((i2,j2), (i, j),di)
-            if otherX ==Opos[0] and otherY == Opos[1] :
-                return (otherX, otherY)
-            return (iUpdate2(otherX), jUpdate2(otherY))
-        elif board[i2][j2] == "O":
-            return (i2, j2)
-        else : 
-            i = i2
-            j = j2
-            continue
-    return (i, j)
 
-# data form : (direction, level)
-start = (null, 0)
+# data form : (rx, ry, bx, by, level)
+start = (rx, ry, bx, by, 0)
 queue.append(start)
-parent[start] = (null, -1)
-R[start] = Rpos
-B[start] = Bpos
 
 ans = -1
 while len(queue) != 0 :
     cur = queue.pop(0)
-    curDir, curLev = cur
-    if curLev != 0 :
-        R[cur] = calMarblePos(R[parent[cur]], B[parent[cur]], curDir)
-        B[cur] = calMarblePos(B[parent[cur]], R[parent[cur]], curDir)
-    if (R[cur], B[cur]) in visited : continue
-    visited.append((R[cur], B[cur]))
-    if R[cur] == Opos and B[cur] != Opos : 
-        ans = curLev
+    rx, ry, bx, by, lev = cur
+    if (rx, ry, bx, by) in visited : continue
+    visited[(rx, ry, bx, by)] = True
+
+    if (rx, ry) == (ox, oy) : 
+        ans = lev
         break
-    elif B[cur] == Opos or curLev >= 10 : continue
+    elif lev >= 10 : continue
     
-    for d in (up, down, right, left) :
-        if curDir == -d : continue
-        if calMarblePos(R[cur], B[cur], d) == R[cur] : continue
-        next = (d, curLev+1)
-        queue.append(next)
-        parent[next] = cur
+    for i in range(4):
+        nrx, nry, rc = calMarblePos(rx, ry, dx[i], dy[i])
+        nbx, nby, bc = calMarblePos(bx, by, dx[i], dy[i])
+        if nbx == ox and nby == oy : continue
+        if nrx == nbx and nry == nby :
+            if rc > bc : 
+                nrx -= dx[i]
+                nry -= dy[i]
+            else :
+                nbx -= dx[i]
+                nby -= dy[i]
+        if (nrx, nry, nbx, nby) in visited : continue
+        queue.append((nrx, nry, nbx, nby, lev+1))
 
 print(ans)
     
