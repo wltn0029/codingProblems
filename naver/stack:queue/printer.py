55ecc40faa6e88e1148ac5d5010379e21e39cfc3
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-16 17:00:44 +0900

    improve readability

--- naver/stack:queue/printer.py
+++ naver/stack:queue/printer.py
@@ -1,16 +1,14 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 1
-    pri = deque()
-    for i, p in enumerate(priorities):
-        pri.append((p, i))
+    answer = 0
+    pri = deque([(p,i) for i, p in enumerate(priorities)])
     
     while len(pri) > 0:
         mv = pri.index(max(pri, key =lambda x : x[0]))
         pri.rotate(-mv)
-        if pri[0][1] == location : 
-            break
-        pri.popleft()
+        p, o = pri.popleft()
         answer+=1
+        if o == location : 
+            break
     return answer
