6dd44567299f5c68cc539170478bd86f36a50682
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-16 17:25:47 +0900

    pass faster

--- naver/stack:queue/trucks.py
+++ naver/stack:queue/trucks.py
@@ -5,7 +5,7 @@ def solution(bridge_length, weight, truck_weights):
     bridge = deque([0 for i in range(bridge_length)])
     weight_sum = 0
     
-    while len(trucks) != 0 or weight_sum != 0:
+    while len(trucks) != 0:
         f = bridge.popleft()
         n = 0
         weight_sum -= f
@@ -16,4 +16,4 @@ def solution(bridge_length, weight, truck_weights):
         bridge.rotate(-1)
         answer+=1
         
-    return answer
+    return answer + bridge_length
