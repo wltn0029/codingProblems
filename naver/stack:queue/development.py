a74af4980c615ea279e1d1697767a67ad39e90e0
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-16 16:03:14 +0900

    linear time solution

--- naver/stack:queue/development.py
+++ naver/stack:queue/development.py
@@ -1,20 +1,17 @@
-from collections import deque
 import math
 
 def solution(progresses, speeds):
-    prog = deque(progresses)
-    sp = deque(speeds)
     answer = []
+    count = 0
+    time = 0
     
-    while len(prog) != 0 :
-        x = math.ceil((100-prog[0])/sp[0])
-        for i, p in enumerate(prog):
-            prog[i] = min(100, p+sp[i]*x)
-        num = 0
-        while len(prog)>0 and prog[0] >= 100 :
-            prog.popleft()
-            sp.popleft()
-            num += 1
-        answer.append(num)
-    
+    for p, s in zip(progresses, speeds):
+        if time*s + p >= 100 :
+            count +=1
+        else :
+            time = math.ceil((100-p)/s)
+            if count>0 : answer.append(count)
+            count = 1
+            
+    answer.append(count)
     return answer
