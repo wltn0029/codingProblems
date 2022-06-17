ff63c78161b84104cfa370c46bc737e2469ccf73
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-17 17:03:39 +0900

    reduce runtime little..

--- naver/bruteForce/test.py
+++ naver/bruteForce/test.py
@@ -1,13 +1,13 @@
 def solution(answers):
-    one = [1,2, 3, 4,5]
-    two = [2,1,2,3,2,4,2,5]
-    three = [3,3,1,1,2,2,4,4,5,5]
-    answer = [[0,1], [0,2], [0,3]]
+    pattern1 =[1,2, 3, 4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    answer = [0, 0, 0]
     
     for i, a in enumerate(answers):
-        if a == one[i%5] : answer[0][0] +=1 
-        if a == two[i%8] : answer[1][0] +=1
-        if a== three[i%10] : answer[2][0] +=1
-    
-    answer.sort(key=lambda x:x[0], reverse=True)
-    return [i for a, i in answer if a == answer[0][0]]
+        if a == pattern1[i%5] : answer[0] +=1 
+        if a == pattern2[i%8] : answer[1] +=1
+        if a== pattern3[i%10] : answer[2] +=1
+        
+    mv = max(answer)
+    return [i+1 for i,a in enumerate(answer) if a == mv]
