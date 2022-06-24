705fa5068c7ab678a39b108d99f81fe3661fe1f0
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-24 17:10:25 +0900

    improve readability

--- naver/bruteForce/prime.py
+++ naver/bruteForce/prime.py
@@ -2,28 +2,13 @@ from itertools import permutations
 import math 
 
 def solution(numbers):
-    nums = list(numbers)
     perms = set()
-    answer = 0
-    for i in range(1, len(nums)+1):
-        for npairs in permutations(nums, i) :
-            num = ""
-            for n in npairs: num+=n
-            perms.add(int(num))
+    for i in range(1, len(numbers)+1):
+        perms|= set(map(int, map("".join, permutations(numbers, i))))
     largest = max(perms)
     
-    prime = [True for _ in range(0,largest+1)]
-    prime[1] = False
-    prime[0] = False
+    perms-=set((0,1))
     for i in range(2, int(math.sqrt(largest))+1):
-        if prime[i] == True:
-            j = 2
-            while i*j <= largest :
-                prime[i*j] = False
-                j += 1
-    
-    for num in perms :
-        if prime[num] :
-            print(num)
-            answer+=1
-    return answer
+        perms-=set(range(i*2, largest+1, i))
+
+    return len(perms)
