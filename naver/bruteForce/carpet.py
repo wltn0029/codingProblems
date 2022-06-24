2ecef24270c88b5bee82ebdcc83d4479ad33cf40
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-24 18:05:28 +0900

    shorten code

--- naver/bruteForce/carpet.py
+++ naver/bruteForce/carpet.py
@@ -1,12 +1,5 @@
 def solution(brown, yellow):
-    factors = []
-    h, w = 0, 0
     for i in range(1, int(yellow**(1/2)) + 1):
-        if yellow%i == 0 : factors.append((i, yellow//i))
-    
-    for h, w in factors:
-        if (2 *(h+w)+4) == brown :
-            break
-            
-    return [w+2, h+2]
-    
+        if yellow%i == 0 : 
+            if(2*(i+yellow//i)+4)==brown:
+                return [yellow//i+2, i+2]
