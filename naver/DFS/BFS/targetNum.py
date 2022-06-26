8cb92f29d307b50785fc2b0ad2abfd548e34190a
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-26 17:44:51 +0900

    use built-in 'product'

--- naver/DFS/BFS/targetNum.py
+++ naver/DFS/BFS/targetNum.py
@@ -1,5 +1,6 @@
+from itertools import product
+
 def solution(numbers, target):
-    if not numbers and  target == 0 : return 1
-    elif not numbers : return 0    
-    else :
-        return solution(numbers[1:], target+numbers[0]) + solution(numbers[1:], target-numbers[0])
+    numbers = [(x, -x) for x in numbers]
+    e = list(map(sum,product(*numbers)))
+    return e.count(target)
