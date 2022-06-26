2405298c4981f7d2af2ceec9eee42c81a515e7d6
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-27 00:45:15 +0900

    Update wordConversion.py
    - use generator to reduce waste of memory
    - use dist to stand out the characteristic of BFS

--- naver/DFS/BFS/wordConversion.py
+++ naver/DFS/BFS/wordConversion.py
@@ -1,35 +1,27 @@
 from collections import deque 
 
+def gen_adjacent(cur, words):
+    for w in words:
+        diff = 0 
+        for c, cmp in zip(cur, w):
+            if c != cmp : diff+=1
+        if diff == 1 :
+            yield w
+
 def solution(begin, target, words):
     if not (target in words) : 
         return 0
     
-    deq = deque()
-    changable = {} # index : list of index
-    words.append(begin)
-    visited = [False]*len(words)
-    wordLen = len(begin)
-    
-    # fill changable 
-    for i, cur in enumerate(words):
-        able = []
-        for j, cmp in enumerate(words):
-            diff=0
-            for w in range(wordLen):
-                if cur[w] != cmp[w] : diff+=1
-            if diff == 1 : able.append(j)
-        changable[i] = able
+    deq = deque([begin])
+    dist = {begin : 0}
     
     # bfs 
-    deq.append((len(words)-1,0))
-    visited[len(words)-1] = True
     while deq :
-        cur, level = deq.popleft()
-        visited[cur] = True
-        if words[cur] == target :
-            return level
-        for cand in changable[cur]:
-            if visited[cand] : continue
-            deq.append((cand, level+1))
+        cur = deq.popleft()
+        if cur == target : return dist[cur]
+        for cand in gen_adjacent(cur, words):
+            if cand in dist : continue
+            deq.append(cand)
+            dist[cand] = dist[cur]+1
             
     return 0
