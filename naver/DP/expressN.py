a3907b109812315ad2b549b878db2844ee5444f5
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-06-30 03:30:01 +0900

    fix some errors
    - string concatenation can be expressed using multiplication
    - floor division for must-int condition

--- naver/DP/expressN.py
+++ naver/DP/expressN.py
@@ -1,22 +1,20 @@
 from collections import defaultdict
 from itertools import product
-from functools import reduce
 
 def solution(N, number):
     dp = defaultdict(set)
     dp[1].add(N)
     
     for n in range(2,9):
-        dp[n].add(int(reduce(lambda acc, cur: acc+cur, [str(N)]*n, "")))
+        dp[n].add(int(str(N)*n))
         for i in range(1, n):
             left = dp[i]
             right = dp[n-i]
             for l, r in product(left,right):
-                if r != 0 : dp[n].add(l/r)
+                if r != 0 : dp[n].add(l//r)
                 dp[n].add(l*r)
                 dp[n].add(l+r)
                 dp[n].add(l-r)
-        
     
     for i in range(1,9):
         if number in dp[i]:
