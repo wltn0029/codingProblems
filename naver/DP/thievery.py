ad87e22489556355e426af2ef804af373d9a0e5d
wltn0029 <46206830+wltn0029@users.noreply.github.com>
2022-07-01 14:39:30 +0900

    implement using bottom-up
    when every level's state should be calculated, bottom-up could be better choice rather than top-down.

--- naver/DP/thievery.py
+++ naver/DP/thievery.py
@@ -5,14 +5,10 @@ def do_dp(money) :
     dp[-1] = money[-1]
     dp[-2] = max(money[-1], money[-2])
     
-    def getMax(i):
-        if dp[i] != -1 : return dp[i]
-        else : 
-            dp[i]=max(getMax(i+2)+money[i], getMax(i+1))
-            return dp[i]
-
-    r = getMax(0)
-    return r
+    for i in range(len(money)-3, -1, -1):
+        dp[i] = max(money[i]+dp[i+2], dp[i+1])
+        
+    return dp[0]
 
 def solution(money):
     if len(money) == 3 : return max(money)
